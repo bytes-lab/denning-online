241e9a9bcb1b9d05379ef4dfc78d99f8dde20033
VD web developer <jason.5001001@gmail.com>
2017-07-15 08:18:04 -0500

    add firm list and verification page

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -218,7 +218,7 @@ materialAdmin
 
     
     })
-    .controller('loginCtrl', ['$rootScope', '$scope', 'Auth', '$window', '$state', function ($rootScope, $scope, Auth, $window, $state) {
+    .controller('loginCtrl', function ($rootScope, $scope, Auth, $window, $state, legalFirmListService) {
 
         // $scope.toggleForgetPassword = function () {
         //     $scope.forgetPassword = !$scope.forgetPassword;
@@ -231,7 +231,131 @@ materialAdmin
         self.login = 1;
         self.register = 0;
         self.forgot = 0;
+        self.verification = 0;
         self.errorMessage = '';
+
+        // legalFirmListService.getList().then(function(data){
+        //     self.legalFirms = [];
+        //     data.forEach(function(item){
+        //         self.legalFirms.push(item);
+        //     })
+        // })
+        self.legalFirms = [{
+            "IDNo": null,
+            "IDType": 0,
+            "address": {
+                "city": "Petaling Jaya",
+                "country": "",
+                "fullAddress": "No. 59B, 1st Floor & 2nd Floor, Jalan SS 6/10, Kelana Jaya47301 Petaling Jaya, Selangor",
+                "line1": "No. 59B, 1st Floor & 2nd Floor, ",
+                "line2": "Jalan SS 6/10, Kelana Jaya",
+                "line3": "",
+                "postcode": "47301",
+                "state": "Selangor"
+            },
+            "code": 4293,
+            "emailAddress": "absidek@tm.net.my",
+            "name": "A B Sidek & Co.",
+            "phoneFax": "(03)7805-2924",
+            "phoneHome": "(03)7805-2920",
+            "phoneMobile": null,
+            "phoneOffice": "(03)7805-2921",
+            "title": null,
+            "webSite": " "
+        },
+        {
+            "IDNo": null,
+            "IDType": 0,
+            "address": {
+                "city": "Kuala Lumpur",
+                "country": "",
+                "fullAddress": "No. Gc, Ground FloorRuby Tower, Og Heights56, Jalan Awan China58200 Kuala Lumpur",
+                "line1": "No. Gc, Ground Floor",
+                "line2": "Ruby Tower, Og Heights",
+                "line3": "56, Jalan Awan China",
+                "postcode": "58200",
+                "state": ""
+            },
+            "code": 5385,
+            "emailAddress": "abteoh@pd.jaring.my",
+            "name": "A B Teoh & Shariza",
+            "phoneFax": "(03)7782-3966",
+            "phoneHome": "(03)7781-5816",
+            "phoneMobile": null,
+            "phoneOffice": null,
+            "title": null,
+            "webSite": " "
+        },
+        {
+            "IDNo": null,
+            "IDType": 0,
+            "address": {
+                "city": "Subang Jaya",
+                "country": "",
+                "fullAddress": "No. 22, Jalan Usj 9/3bUep Subang Jaya47620 Subang Jaya, Selangor",
+                "line1": "No. 22, Jalan Usj 9/3b",
+                "line2": "Uep Subang Jaya",
+                "line3": "",
+                "postcode": "47620",
+                "state": "Selangor"
+            },
+            "code": 923,
+            "emailAddress": " ",
+            "name": "A D Rajah",
+            "phoneFax": "(03)5638-9957",
+            "phoneHome": "(03)5638-8744",
+            "phoneMobile": null,
+            "phoneOffice": null,
+            "title": null,
+            "webSite": " "
+        },
+        {
+            "IDNo": null,
+            "IDType": 0,
+            "address": {
+                "city": "Klang",
+                "country": "",
+                "fullAddress": "No. 14-B, Jalan Istana41000 Klang, Selangor",
+                "line1": "No. 14-B, Jalan Istana",
+                "line2": "",
+                "line3": "",
+                "postcode": "41000",
+                "state": "Selangor"
+            },
+            "code": 924,
+            "emailAddress": " ",
+            "name": "A D Rajah",
+            "phoneFax": "(03)3373-8525",
+            "phoneHome": "(03)3371-8785",
+            "phoneMobile": null,
+            "phoneOffice": "(03)3372-8810",
+            "title": null,
+            "webSite": " "
+        },
+        {
+            "IDNo": null,
+            "IDType": 0,
+            "address": {
+                "city": "Ipoh",
+                "country": "",
+                "fullAddress": "No.73 B, 1st FloorJalan Sultan Yusuff,30000 Ipoh, Perak",
+                "line1": "No.73 B, 1st Floor",
+                "line2": "Jalan Sultan Yusuff,",
+                "line3": "",
+                "postcode": "30000",
+                "state": "Perak"
+            },
+            "code": 2576,
+            "emailAddress": " ",
+            "name": "A Damadram Mariah & Associates(closed)",
+            "phoneFax": "(05)2410-259",
+            "phoneHome": "(05)2410-258",
+            "phoneMobile": null,
+            "phoneOffice": null,
+            "title": null,
+            "webSite": " "
+        }];
+            
         self.doLogin = function (userData) {
 
             return Auth.login(userData.email, userData.password)
@@ -248,7 +372,7 @@ materialAdmin
                 self.doLogin(self.user);
             }
         }
-    }])
+    })
 
     // =========================================================================
     // Header
