46730e5ab31b32267a8eef50b8194f59715bb0c1
VD web developer <jason.5001001@gmail.com>
2017-07-25 12:49:40 -0500

    finish building

--- js/controllers/building.js
+++ js/controllers/building.js
@@ -25,22 +25,20 @@ materialAdmin
                     var orderedData = params.filter() ? $filter('filter')(self.data, params.filter()) : self.data;
                     orderedData = params.sorting() ? $filter('orderBy')(orderedData, params.orderBy()) : orderedData;
 
-                    this.new_ic = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
-                    this.id_type = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
-                    this.name = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
-                    this.email = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
-                    this.phone3 = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+                    this.code = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+                    this.bc_type = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+                    this.type = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
 
                     params.total(orderedData.length); // set total for recalc pagination
-                    return this.new_ic, this.id_type, this.name, this.email, this.phone3;
+                    return this.code, this.bc_type, this.type;
                 }
             })      
         }
 
-        self.modalContent = 'Are you sure to delete the contract?';
+        self.modalContent = 'Are you sure to delete the building?';
     
         //Create Modal
-        function modalInstances(animation, size, backdrop, keyboard, contact) {
+        function modalInstances(animation, size, backdrop, keyboard, building) {
             var modalInstance = $uibModal.open({
                 animation: animation,
                 templateUrl: 'myModalContent.html',
@@ -49,28 +47,28 @@ materialAdmin
                 backdrop: backdrop,
                 keyboard: keyboard,
                 resolve: {
-                    contact: function () {
-                        return contact;
+                    building: function () {
+                        return building;
                     }
                 }
             
             });
         }
         //Prevent Outside Click
-        function openDelete(contact) {
-            modalInstances(true, '', 'static', true, contact)
+        function openDelete(building) {
+            modalInstances(true, '', 'static', true, building)
         };        
     })
 
-    .controller('buildingDeleteModalCtrl', function ($scope, $modalInstance, contact, buildingService, $state) {
+    .controller('buildingDeleteModalCtrl', function ($scope, $modalInstance, building, buildingService, $state) {
         $scope.ok = function () {
-            buildingService.delete(contact).then(function(contact) {
+            buildingService.delete(building).then(function(building) {
                 $state.reload();
             })
             .catch(function(err){
                 //Handler
 
-                //$scope.formname.contactInfo.$error.push({meessage:''});
+                //$scope.formname.buildingInfo.$error.push({meessage:''});
             });
             $modalInstance.close();
         };
@@ -87,21 +85,21 @@ materialAdmin
         if($stateParams.id) {
             buildingService.getItem($stateParams.id)
             .then(function(item){
-                self.contact = item;
+                self.building = item;
             });
         } else {
-            self.contact = {};
+            self.building = {};
         }
 
         function save() {
-            buildingService.save(self.contact).then(function(contact) {
-                self.contact = contact;
-                $state.go('contacts.list');
+            buildingService.save(self.building).then(function(building) {
+                self.building = building;
+                $state.go('buildings.list');
             })
             .catch(function(err){
                 //Handler
 
-                //$scope.formname.contactInfo.$error.push({meessage:''});
+                //$scope.formname.buildingInfo.$error.push({meessage:''});
             });
         }
     })
--- js/services/building.js
+++ js/services/building.js
@@ -7,228 +7,168 @@ materialAdmin
         var service = {};
         var fakedata = [
             {
-                "id": 10238,
-                "name": "Marc Barnes",
+                "code": 10238,
+                "type": "Marc Barnes",
                 "email": "marc.barnes54@example.com",
-                "new_ic": "760105-06-2540",
+                "code": "760105-06-2540",
                 "phone3": "(382)-122-5003",
-
-                "id_type": "New + Old IC",
-                "old_ic": "A33440078",
-                "title": "Mr.",
-                "address1": " 178, Kampung Baru,",
-                "address2": "",
-                "address3": "",
-                "postcode": "28300",
-                "town": "Mentakab",
-                "state": "Pahang",
-                "country": "Malaysia",
-                "phone1": "(09)2554-558",
-                "phone2": "",
-                "fax": "",
-                "website": "",
-                "citizenship": "Malaysia Citizen",
-                "dob": "02/04/1976",
-                "marital_status": "",
-                "num_children": "",
-                "occupation": "Rubber Tapper",
-                "tax_file_no": "Tax4235",
-                "IRD_branch": "Alor Setar",
-                "customer_type": "",
-                "government_department": "",
-                "GST_status_verified": true,
-                "last_GST_status_verified_date": "",
-                "GST_registered": true,
-                "GST_reg_no": "",
-                "RMCD_approval_no": "",
-                "mailing_list": "",
-                "newsletter": true,
-                "office_address": "",
-                "director1": "",
-                "director2": "",
-                "secretary": "",
-                "contact_person": "Contact Me",
-                "spouse_name": "",
-                "spouse_occupation": "",
-                "race": "",
-                "religion": "",
-                "sex": "",
-                "monthly_income": "USD3,000",
-                "place_of_work": "",
-                "education_level": "",
-                "institution": "",
-                "extra1": "",
-                "extra2": "",
-                "extra3": "",
-                "extra4": "",
-                "extra5": "",
-                "corespondence_address": "",
-                "extra_address1": "",
-                "extra_address2": "",
-                "extra_address3": "",
-                "entered_by": "APP test",
-                "entered_date": "27/06/2017",
-                "updated_by": "",
-                "updated_date": "",
-                "add_to_favorites": false,
-                "remarks": "",
-                "deceased": false
+                "bc_type": "New + Old IC"
             },
             {   
-                "id": 10243,
-                "name": "Glen Curtis",
+                "code": 10243,
+                "type": "Glen Curtis",
                 "email": "glen.curtis11@example.com",
-                "new_ic": "760105-06-2520",
-                "id_type": "New + Old IC",
+                "code": "760105-06-2520",
+                "bc_type": "New + Old IC",
                 "phone3": "(477)-981-4948"
             },
             {
-                "id": 10248,
-                "name": "Beverly Gonzalez",
+                "code": 10248,
+                "type": "Beverly Gonzalez",
                 "email": "beverly.gonzalez54@example.com",
-                "new_ic": "760105-06-2330",
-                "id_type": "New + Old IC",
+                "code": "760105-06-2330",
+                "bc_type": "New + Old IC",
                 "phone3": "(832)-255-5161"
             },
             {
-                "id": 10253,
-                "name": "Yvonne Chavez",
+                "code": 10253,
+                "type": "Yvonne Chavez",
                 "email": "yvonne.chavez@example.com",
-                "new_ic": "760105-32-2540",
-                "id_type": "New + Old IC",
+                "code": "760105-32-2540",
+                "bc_type": "New + Old IC",
                 "phone3": "(477)-446-3715"
             },
             {
-                "id": 10234,
-                "name": "Melinda Mitchelle",
+                "code": 10234,
+                "type": "Melinda Mitchelle",
                 "email": "melinda@example.com",
-                "new_ic": "778805-06-2540",
-                "id_type": "New + Old IC",
+                "code": "778805-06-2540",
+                "bc_type": "New + Old IC",
                 "phone3": "(813)-716-4996"
                 
             },
             {
-                "id": 10239,
-                "name": "Shannon Bradley",
+                "code": 10239,
+                "type": "Shannon Bradley",
                 "email": "shannon.bradley42@example.com",
-                "new_ic": "2120105-06-2540",
-                "id_type": "New + Old IC",
+                "code": "2120105-06-2540",
+                "bc_type": "New + Old IC",
                 "phone3": "(774)-291-9928"
             },
             {
-                "id": 10244,
-                "name": "Virgil Kim",
+                "code": 10244,
+                "type": "Virgil Kim",
                 "email": "virgil.kim81@example.com",
-                "new_ic": "760005-06-2540",
-                "id_type": "New + Old IC",
+                "code": "760005-06-2540",
+                "bc_type": "New + Old IC",
                 "phone3": "(219)-181-7898"
             },
             {
-                "id": 10249,
-                "name": "Letitia Robertson",
+                "code": 10249,
+                "type": "Letitia Robertson",
                 "email": "letitia.rober@example.com",
-                "new_ic": "540105-06-2540",
+                "code": "540105-06-2540",
                 "phone3": "(647)-209-4589"
             },
             {
-                "id": 10237,
-                "name": "Claude King",
+                "code": 10237,
+                "type": "Claude King",
                 "email": "claude.king22@example.com",
-                "new_ic": "98105-06-2540",
-                "id_type": "New + Old IC",
+                "code": "98105-06-2540",
+                "bc_type": "New + Old IC",
                 "phone3": "(657)-988-8701"
             },
             {
-                "id": 10242,
-                "name": "Roland Craig",
+                "code": 10242,
+                "type": "Roland Craig",
                 "email": "roland.craig47@example.com",
-                "new_ic": "767105-06-2540",
-                "id_type": "New + Old IC",
+                "code": "767105-06-2540",
+                "bc_type": "New + Old IC",
                 "phone3": "(932)-935-9471"
             },
             {
-                "id": 10247,
-                "name": "Colleen Parker",
+                "code": 10247,
+                "type": "Colleen Parker",
                 "email": "colleen.parker38@example.com",
-                "new_ic": "762225-06-2540",
-                "id_type": "New + Old IC",
+                "code": "762225-06-2540",
+                "bc_type": "New + Old IC",
                 "phone3": "(857)-459-2792"
             },
             {
-                "id": 10252,
-                "name": "Leah Jensen",
+                "code": 10252,
+                "type": "Leah Jensen",
                 "email": "leah.jensen27@example.com",
-                "new_ic": "76087-06-2540",
-                "id_type": "New + Old IC",
+                "code": "76087-06-2540",
+                "bc_type": "New + Old IC",
                 "phone3": "(861)-275-4686"
             },
             {
-                "id": 10236,
-                "name": "Harold Martinez",
+                "code": 10236,
+                "type": "Harold Martinez",
                 "email": "martinez67@example.com",
-                "new_ic": "960105-96-2520",
-                "id_type": "New + Old IC",
+                "code": "960105-96-2520",
+                "bc_type": "New + Old IC",
                 "phone3": "(836)-634-9133"
             },
             {
-                "id": 10241,
-                "name": "Keith Lowe",
+                "code": 10241,
+                "type": "Keith Lowe",
                 "email": "keith.lowe96@example.com",
-                "new_ic": "760105-06-2550",
-                "id_type": "New + Old IC",
+                "code": "760105-06-2550",
+                "bc_type": "New + Old IC",
                 "phone3": "(778)-787-3100"
             },
             {
-                "id": 10246,
-                "name": "Charles Walker",
+                "code": 10246,
+                "type": "Charles Walker",
                 "email": "charles.walker90@example.com",
-                "new_ic": "760105-26-2540",
-                "id_type": "New + Old IC",
+                "code": "760105-26-2540",
+                "bc_type": "New + Old IC",
                 "phone3": "(486)-440-4716"
             },
             {
-                "id": 10251,
-                "name": "Lillie Curtis",
+                "code": 10251,
+                "type": "Lillie Curtis",
                 "email": "lillie.curtis12@example.com",
-                "new_ic": "760605-06-2540",
-                "id_type": "New + Old IC",
+                "code": "760605-06-2540",
+                "bc_type": "New + Old IC",
                 "phone3": "(342)-510-2258"
             },
             {
-                "id": 10235,
-                "name": "Genesis Reynolds",
+                "code": 10235,
+                "type": "Genesis Reynolds",
                 "email": "genesis@example.com",
-                "new_ic": "760105-07-2540",
-                "id_type": "New + Old IC",
+                "code": "760105-07-2540",
+                "bc_type": "New + Old IC",
                 "phone3": "(339)-375-1858"
             },
             {
-                "id": 10240,
-                "name": "Oscar Palmer",
+                "code": 10240,
+                "type": "Oscar Palmer",
                 "email": "oscar.palmer24@example.com",
-                "new_ic": "760105-76-2540",
-                "id_type": "New + Old IC",
+                "code": "760105-76-2540",
+                "bc_type": "New + Old IC",
                 "phone3": "(544)-270-9912"
             },
             {
-                "id": 10245,
-                "name": "Lena Bishop",
+                "code": 10245,
+                "type": "Lena Bishop",
                 "email": "Lena Bishop",
-                "new_ic": "550105-06-2540",
-                "id_type": "New + Old IC",
+                "code": "550105-06-2540",
+                "bc_type": "New + Old IC",
                 "phone3": "(177)-521-1556"
             },
             {
-                "id": 10250,
-                "name": "Kent Nguyen",
+                "code": 10250,
+                "type": "Kent Nguyen",
                 "email": "kent.nguyen34@example.com",
-                "new_ic": "460105-06-2540",
-                "id_type": "New + Old IC",
+                "code": "460105-06-2540",
+                "bc_type": "New + Old IC",
                 "phone3": "(506)-533-6801"
             }
         ];
 
-        service.contacts = null;
+        service.buildings = null;
         service.getList = getList;
         service.getItem = getItem;
         service.save = save;
@@ -237,31 +177,31 @@ materialAdmin
         function getList() {
             var deferred = $q.defer();
 
-            if(service.contacts)
-                deferred.resolve(service.contacts);
+            if(service.buildings)
+                deferred.resolve(service.buildings);
             else
                 $timeout(function(){
                     deferred.resolve(fakedata);
-                    service.contacts = fakedata;                
+                    service.buildings = fakedata;                
                 }, 100);
             return deferred.promise;
         }  
 
-        function getItem(new_ic) {
+        function getItem(code) {
 
             var deferred = $q.defer();
 
             $timeout(function(){
-                if (!service.contacts) {
-                    service.contacts = fakedata;
+                if (!service.buildings) {
+                    service.buildings = fakedata;
                 }
-                var item = service.contacts.filter(function(c){
-                    return c.new_ic == new_ic;
+                var item = service.buildings.filter(function(c){
+                    return c.code == code;
                 });
                 if (item.length == 1)
                     deferred.resolve(item[0]);
                 else
-                    deferred.resolve(new Error('No Item with the new_ic'));
+                    deferred.resolve(new Error('No Item with the code'));
                 
                 
             }, 100);
@@ -269,16 +209,16 @@ materialAdmin
             return deferred.promise;
         }
 
-        function save(contact) {
+        function save(building) {
             var deferred = $q.defer();
 
             $timeout(function(){
-                var idx = service.contacts.map(function(c) {return c.new_ic; }).indexOf(contact.new_ic);
+                var idx = service.buildings.map(function(c) {return c.code; }).indexOf(building.code);
                 if(idx != -1) {
-                    service.contacts[idx] = contact;
-                    deferred.resolve(contact);
+                    service.buildings[idx] = building;
+                    deferred.resolve(building);
                 } else {
-                    service.contacts.push(contact);
+                    service.buildings.push(building);
                 }
                 //defered.reject(new Error('dd'));
             }, 100);
@@ -286,16 +226,16 @@ materialAdmin
             return deferred.promise;
         }
 
-        function delete_(contact) {
+        function delete_(building) {
             var deferred = $q.defer();
 
             $timeout(function(){
-                var idx = service.contacts.map(function(c) {return c.new_ic; }).indexOf(contact.new_ic);
+                var idx = service.buildings.map(function(c) {return c.code; }).indexOf(building.code);
                 if(idx != -1) {
-                    service.contacts.splice(idx, 1);
-                    deferred.resolve(contact);
+                    service.buildings.splice(idx, 1);
+                    deferred.resolve(building);
                 } else {
-                    defered.reject(new Error('There is no such contact'));
+                    defered.reject(new Error('There is no such building'));
                 }
             }, 100);
 
