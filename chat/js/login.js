970de44c9a8961e942db8c9627f8f284e4ea1faa
VD web developer <jason.5001001@gmail.com>
2017-09-01 16:05:38 -0500

    add chat

---
+++ chat/js/login.js
@@ -0,0 +1,136 @@
+'use strict';
+
+function Login() {
+    this.isLoginPageRendered = false;
+    this.isLogin = false;
+}
+
+Login.prototype.init = function(){
+    var self = this;
+
+    return new Promise(function(resolve, reject) {
+        var user = {
+            login: 'yanhongho@hotmail.com',
+            // login: 'tmho@hotmail.com',
+            password: 'denningIT'
+        };
+
+        if(user && !app.user){
+            app.room = user.tag_list;
+            self.login(user)
+                .then(function(){
+                    resolve(true);
+                }).catch(function(error){
+                reject(error);
+            });
+        } else {
+            resolve(false);
+        }
+    });
+};
+
+Login.prototype.login = function (user) {
+    var self = this;
+    return new Promise(function(resolve, reject) {
+        if(self.isLoginPageRendered){
+            document.forms.loginForm.login_submit.innerText = 'loading...';
+        } else {
+            self.renderLoadingPage();
+        }
+        QB.createSession(function(csErr, csRes) {
+            var userRequiredParams = {
+                'login':user.login,
+                'password': user.password
+            };
+            if (csErr) {
+                loginError(csErr);
+            } else {
+                app.token = csRes.token;
+                QB.login(userRequiredParams, function(loginErr, loginUser){
+                    if(loginErr) {                        
+                        alert('Login Failed');
+                    } else {
+                        loginSuccess(loginUser);
+                    }
+                });
+            }
+        });
+
+        function loginSuccess(userData){
+            app.user = userModule.addToCache(userData);
+            app.user.user_tags = userData.user_tags;
+            QB.chat.connect({userId: app.user.id, password: user.password}, function(err, roster){
+                if (err) {
+                    document.querySelector('.j-login__button').innerText = 'Login';
+                    console.error(err);
+                    reject(err);
+                } else {
+                    self.isLogin = true;
+                    resolve();
+                }
+            });
+        }
+
+        function loginError(error){
+            self.renderLoginPage();
+            console.error(error);
+            alert(error + "\n" + error.detail);
+            reject(error);
+        }
+    });
+};
+
+Login.prototype.renderLoginPage = function(){
+    helpers.clearView(app.page);
+
+    app.page.innerHTML = helpers.fillTemplate('tpl_login', {
+        version: QB.version
+    });
+    this.isLoginPageRendered = true;
+    this.setListeners();
+};
+
+Login.prototype.renderLoadingPage = function(){
+    helpers.clearView(app.page);
+    app.page.innerHTML = helpers.fillTemplate('tpl_loading');
+};
+
+Login.prototype.setListeners = function(){
+    var self = this,
+        loginForm = document.forms.loginForm,
+        formInputs = [loginForm.userName, loginForm.userGroup],
+        loginBtn = loginForm.login_submit;
+
+    // add event listeners for each input;
+    _.each(formInputs, function(i){
+        i.addEventListener('focus', function(e){
+            var elem = e.currentTarget,
+                container = elem.parentElement;
+
+            if (!container.classList.contains('filled')) {
+                container.classList.add('filled');
+            }
+        });
+
+        i.addEventListener('focusout', function(e){
+            var elem = e.currentTarget,
+                container = elem.parentElement;
+
+            if (!elem.value.length && container.classList.contains('filled')) {
+                container.classList.remove('filled');
+            }
+        });
+
+        i.addEventListener('input', function(){
+            var userName = loginForm.userName.value.trim(),
+                userGroup = loginForm.userGroup.value.trim();
+            if(userName.length >=3 && userGroup.length >= 3){
+                loginBtn.removeAttribute('disabled');
+            } else {
+                loginBtn.setAttribute('disabled', true);
+            }
+        })
+    });
+};
+
+var loginModule = new Login();
