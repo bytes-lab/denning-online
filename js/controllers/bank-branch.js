7ed7ad98d7d24b66903d2098de2d7bd0ab0d285d
IT-corridor <jason.5001001@gmail.com>
2018-03-31 18:35:46 +0530

    finish bank branch, revise contact

--- js/controllers/bank-branch.js
+++ js/controllers/bank-branch.js
@@ -1,64 +1,42 @@
 materialAdmin
   .controller('bankBranchListCtrl', function($filter, $uibModal, NgTableParams, bankBranchService, Auth, $state) {
     var self = this;
-    self.dataReady = false;
+    self.search = search;
+    self.keyword = '';
     self.clickHandler = clickHandler;
-
-    bankBranchService.getList().then(function(data) {
-      self.data = data;
-      self.dataReady = true;
-      initializeTable();
-    });    
     
     function clickHandler(item) {
       $state.go('bank-branches.edit', {'id': item.code});
     }
 
-    function initializeTable () {
-      //Filtering
-      self.tableFilter = new NgTableParams({
-        page: 1,                // show first page
-        count: 25,
-        sorting: {
-          name: 'asc'           // initial sorting
-        }
-      }, {
-        total: self.data.length,  // length of data
-        getData: function(params) {
-          // use built-in angular filter
-          var orderedData = params.filter() ? $filter('filter')(self.data, params.filter()) : self.data;
-          orderedData = params.sorting() ? $filter('orderBy')(orderedData, params.orderBy()) : orderedData;
-          params.total(orderedData.length); // set total for recalc pagination
-          return orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
-        }
-      })    
-    } 
+    self.tableFilter = new NgTableParams({
+      page: 1,                // show first page
+      count: 25,
+      sorting: {
+        name: 'asc'           // initial sorting
+      }
+    }, {
+      getData: function(params) {
+        return bankBranchService.getList(params.page(), params.count(), self.keyword).then(function(data) {
+          params.total(data.headers('x-total-count'));
+          return data.data;
+        });
+      }
+    })    
+
+    function search() {
+      self.tableFilter.reload();
+    }
   })
 
   .controller('bankBranchEditCtrl', function($filter, $uibModal, $stateParams, bankBranchService, $state, Auth) {
     var self = this;
-    self.save = save;
     self.cancel = cancel;
-    self.isDialog = false;
-    self.viewMode = false;  // for edit / create
-    self.userInfo = Auth.getUserInfo();
-    self.can_edit = false;
-    self.queryFields = queryFields;
 
-    function queryFields(field, searchText) {
-      return self[field].filter(function(c) {
-        return c.description.search(new RegExp(searchText, "i")) > -1;
-      });
-    }
-
-    if ($stateParams.id) {
-      bankBranchService.getItem($stateParams.id)
-      .then(function(item){
-        self.bankBranch = angular.copy(item);  // important
-      });
-    } else {
-      self.bankBranch = {};
-    }
+    bankBranchService.getItem($stateParams.id)
+    .then(function(item){
+      self.bankBranch = angular.copy(item);
+    });
 
     function cancel() {
       $state.go('bank-branches.list');
--- js/services/bank-branch.js
+++ js/services/bank-branch.js
@@ -3,24 +3,22 @@ materialAdmin
   // Bank Branches
   // =========================================================================
   
-  .service('bankBranchService', function($q, $timeout, $http, Auth) {
+  .service('bankBranchService', function($http, Auth) {
     var service = {};
 
     service.bankBranches = null;
     service.getList = getList;
     service.getItem = getItem;
-    service.save = save;
-    service.delete = delete_;
     service.headers = Auth.isAuthenticated();
 
-    function getList() {
+    function getList(page, pagesize, keyword) {
       return $http({
         method: 'GET',
-        url: 'http://43.252.215.81/denningwcf/v1/bank/Branch',
+        url: 'http://43.252.215.81/denningwcf/v1/bank/Branch?page='+page+'&pagesize='+pagesize+'&search='+keyword,
         headers: service.headers
       }).then(function(resp) {
         service.bankBranches = resp.data;
-        return resp.data;
+        return resp;
       });  
     }
 
@@ -32,41 +30,6 @@ materialAdmin
       }).then(function(resp) {
         return resp.data;
       });  
-    }
-
-    function save(contact) {
-      var method = contact.code ? 'PUT': 'POST';
-      delete contact.relatedMatter;
-      contact.title = contact.title.description;
-      // contact.irdBranch = contact.irdBranch.description;
-
-      console.log(contact);
-
-      return $http({
-        method: method,
-        url: 'http://43.252.215.81/denningwcf/v1/bank/Branch',
-        headers: service.headers,
-        data: contact
-      }).then(function(response) {
-        return response.data;
-      });
-    }
-
-    function delete_(contact) {
-      var deferred = $q.defer();
-
-      $timeout(function(){
-        var idx = service.bankBranches.map(function(c) { return c.code; }).indexOf(contact.code);
-        if(idx != -1) {
-          service.bankBranches.splice(idx, 1);
-          deferred.resolve(contact);
-        } else {
-          deferred.reject(new Error('There is no such bank branch'));
-        }
-        // @@ send delete request to server to delete the item
-      }, 100);
-
-      return deferred.promise;
     }
     return service;
   })
