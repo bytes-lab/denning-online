f15770ee73c80b28d24ac5d1b4dd306b1354d9b5
IT-corridor <jason.5001001@gmail.com>
2018-04-11 09:41:30 +0530

    finish ledger level2

--- js/controllers/ledger.js
+++ js/controllers/ledger.js
@@ -36,21 +36,61 @@ materialAdmin
     }  
   })
 
-  .controller('accountList2Ctrl', function($stateParams, NgTableParams, ledgerService, $state) {
+  .controller('accountList2Ctrl', function($stateParams, NgTableParams, ledgerService, $state, $q) {
     var self = this;
     self.fileNo = $stateParams.fileNo;
     self.ledger_level2 = ledger_level2;
-    self.category = $stateParams.category;
+    self.toggleSelection = toggleSelection;
+    self.toggleAll = toggleAll;
+    self.all_categories = [{'key': 'client', 'val': 'Client'}, 
+      {'key': 'fd', 'val': 'FD'}, 
+      {'key': 'disb', 'val': 'Disbursement'}, 
+      {'key': 'advance', 'val': 'Advanced'}, 
+      {'key': 'other', 'val': 'Other'}];
+    self.select_all = false;
+    self.categories = [$stateParams.category];
+
+    function toggleAll() {
+      if (self.select_all) {
+        self.categories = ['client', 'fd', 'disb', 'advance', 'other'];
+      } else {
+        self.categories = [];
+      }
+      // console.log(self.categories);
+      self.set_category();
+    }
+
+    function toggleSelection(category) {
+      var idx = self.categories.indexOf(category);
+      if (idx > -1) {
+        self.categories.splice(idx, 1);
+        self.select_all = false;
+      } else {
+        self.categories.push(category);
+      }
+      // console.log(self.categories);
+      self.set_category();
+    }
 
     function ledger_level2(item) {
       $state.go('notes.edit', {'fileNo': $stateParams.fileNo, 'id': item.code});
     }
 
-    self.set_category = function(category) {
+    self.set_category = function() {
       self.dataReady = false;
-      ledgerService.getList($stateParams.fileNo, category).then(function(data) {
+
+      var promises = self.categories.map(function(category) {
+        return ledgerService.getList($stateParams.fileNo, category);
+      })
+      
+      $q.all(promises).then(function(data) {
+        var allData = [];
+        data.map(function(datum){
+          allData = allData.concat(datum)
+        });
+
         self.data = [];
-        angular.forEach(data, function(value, key) {
+        angular.forEach(allData, function(value, key) {
           value['date'] = value['date'].split(' ')[0];
           self.data.push(value);
         });
@@ -60,7 +100,7 @@ materialAdmin
       });    
     };
     
-    self.set_category($stateParams.category);
+    self.set_category();
 
     function initializeTable () {
       //Filtering
