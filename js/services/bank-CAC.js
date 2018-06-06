8a0605bcdc2bffd66d40c18352ef9b161edcc52b
IT-corridor <jason.5001001@gmail.com>
2018-06-07 03:47:36 +0530

    finish bank CAC list

--- js/services/bank-CAC.js
+++ js/services/bank-CAC.js
@@ -6,7 +6,7 @@ materialAdmin
   .service('bankCACService', function($http, Auth) {
     var service = {};
 
-    service.getTableList = function (page, pagesize, keyword) {
+    service.getTableList = function (page, pagesize, keyword='') {
       return $http({
         method: 'GET',
         url: 'http://43.252.215.81/denningwcf/v1/table/BankCACCode?page='+page+'&pagesize='+pagesize+'&search='+keyword,
