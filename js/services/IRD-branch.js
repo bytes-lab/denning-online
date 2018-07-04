b41545d570d2681910968226751e96070beaa8bd
IT-corridor <jason.5001001@gmail.com>
2018-07-05 04:41:30 +0530

    revising url structure for IRD branch

--- js/services/IRD-branch.js
+++ js/services/IRD-branch.js
@@ -3,39 +3,23 @@ materialAdmin
   // IRB Branches
   // =========================================================================
   
-.service('IRDBranchService', function($http, Auth){
-  var service = {
-    getList: getList,
-    getItem: getItem,
-    headers: Auth.isAuthenticated()
-  };
+.service('IRDBranchService', function(http){
+  var service = {};
 
-  function getList(page=1, pagesize=20, search='') {
-    return $http({
-      method: 'GET',
-      url: 'https://43.252.215.81/denningwcf/v1/IRDBranch?page='+page+'&pagesize='+pagesize+'&search='+search,
-      headers: service.headers
-    }).then(function(resp) {
+  service.getList = function (page=1, pagesize=20, search='') {
+    return http.GET('/v1/IRDBranch?page='+page+'&pagesize='+pagesize+'&search='+search).then(function(resp) {
       return resp.data;
     });
   }
 
-  function getTableList(page=1, pagesize=20, search='') {
-    return $http({
-      method: 'GET',
-      url: 'https://43.252.215.81/denningwcf/v1/table/IRDBranch?page='+page+'&pagesize='+pagesize+'&search='+search,
-      headers: service.headers
-    }).then(function(resp) {
+  service.getTableList = function (page=1, pagesize=20, search='') {
+    return http.GET('/v1/table/IRDBranch?page='+page+'&pagesize='+pagesize+'&search='+search).then(function(resp) {
       return resp.data;
     });
   }
 
-  function getItem(code) {
-    return $http({
-      method: 'GET',
-      url: 'https://43.252.215.81/denningwcf/v1/table/IRDBranch/'+code,
-      headers: service.headers
-    }).then(function(resp) {
+  service.getItem = function (code) {
+    return http.GET('/v1/table/IRDBranch/'+code).then(function(resp) {
       return resp.data;
     });
   }
