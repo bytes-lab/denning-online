b7a9ac98c3ce5bbb190c09b24378f948bfcca1c4
IT-corridor <jason.5001001@gmail.com>
2018-09-19 15:53:24 +0530

    revise preset bill edit page

--- js/controllers/presetbill.js
+++ js/controllers/presetbill.js
@@ -23,7 +23,8 @@ denningOnline
   })
 
   .controller('presetbillEditCtrl', function($filter, $stateParams, presetbillService, 
-                                             $state, billingitemService, refactorService) 
+                                             $state, billingitemService, refactorService,
+                                             NgTableParams) 
   {
     var self = this;
     self.isDialog = false;
@@ -31,6 +32,8 @@ denningOnline
     billingitemService.getStateList().then(function (resp) {
       self.states = resp.data;
     })
+
+    self.itemType = 'All';
 
     self.categories = [
       'Conveyancing',
@@ -46,11 +49,33 @@ denningOnline
       'Common'
     ];
 
+    function initializeTable () {
+      self.tableFilter = new NgTableParams({
+        page: 1,
+        count: 25,
+        sorting: {
+          name: 'asc' 
+        }
+      }, {
+        getData: function (params) {
+          return self.entity.listBilledItems.filter(function (item) {
+            return self.itemType == 'All' || item.strBillItemType == self.itemType;
+          })
+        } 
+      })
+    }
+
+    self.filterItem = function (type) {
+      self.itemType = type;
+      self.tableFilter.reload();
+    }
+
     if ($stateParams.id) {
       self.title = 'PRESET BILL EDIT';
       presetbillService.getItem($stateParams.id).then(function (item){
         self.entity = refactorService.preConvert(item, true);
         self.entity_ = angular.copy(self.entity);
+        initializeTable();
       });
     } else {
       self.title = 'NEW PRESET BILL';
