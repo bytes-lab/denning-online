24646f8af93d8ecb9bc700d095938e20b07dcb48
IT-corridor <jason.5001001@gmail.com>
2018-09-24 20:00:27 +0530

    finish receipt list finish

--- js/controllers/receipt.js
+++ js/controllers/receipt.js
@@ -1,104 +1,224 @@
 denningOnline
-    .controller('receiptListCtrl', function($filter, $uibModal, NgTableParams, receiptService) {
-        var self = this;
-        self.dataReady = false;
-        self.openDelete = openDelete;
+  .controller('receiptListCtrl', function(NgTableParams, receiptService, Auth, $state) {
+    var self = this;
+    self.userInfo = Auth.getUserInfo();
 
-        receiptService.getList().then(function(data) {
-            self.data = data;
-            self.dataReady = true;
-            initializeTable();
-        });        
-        
-        function initializeTable () {
-            //Filtering
-            self.tableFilter = new NgTableParams({
-                page: 1,            // show first page
-                count: 10,
-                sorting: {
-                    name: 'asc'     // initial sorting
-                }
-            }, {
-                total: self.data.length, // length of data
-                getData: function(params) {
-                    // use build-in angular filter
-                    var orderedData = params.filter() ? $filter('filter')(self.data, params.filter()) : self.data;
-                    orderedData = params.sorting() ? $filter('orderBy')(orderedData, params.orderBy()) : orderedData;
-                    params.total(orderedData.length); // set total for recalc pagination
-                    return orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
-                }
-            })      
-        }
-
-        //Create Modal
-        function modalInstances(animation, size, backdrop, keyboard, receipt) {
-            var modalInstance = $uibModal.open({
-                animation: animation,
-                templateUrl: 'myModalContent.html',
-                controller: 'ReceiptDeleteModalCtrl',
-                size: size,
-                backdrop: backdrop,
-                keyboard: keyboard,
-                resolve: {
-                    receipt: function () {
-                        return receipt;
-                    }
-                }            
-            });
-        }
-
-        //Prevent Outside Click
-        function openDelete(receipt) {
-            modalInstances(true, '', 'static', true, receipt)
-        };        
+    self.tableFilter = new NgTableParams({
+      page: 1,
+      count: 25,
+    }, {
+      getData: function(params) {
+        return receiptService.getList(params.page(), params.count(), self.keyword)
+        .then(function (data) {
+          params.total(data.headers('x-total-count'));
+          return data.data;
+        });
+      }
     })
 
-    .controller('ReceiptDeleteModalCtrl', function ($scope, $uibModalInstance, receipt, receiptService, $state) {
-        $scope.ok = function () {
-            receiptService.delete(receipt).then(function(receipt) {
-                $state.reload();
-            })
-            .catch(function(err){
-                //Handler
+    self.search = function () {
+      self.tableFilter.reload();
+    }
+  })
 
-                //$scope.formname.receiptInfo.$error.push({meessage:''});
-            });
-            $uibModalInstance.close();
-        };
 
-        $scope.cancel = function () {
-            $uibModalInstance.close();
-            $state.go('receipts.list');
-        };
-    })
+  .controller('receiptEditCtrl', function($stateParams, receiptService, $state, Auth,
+                                          refactorService, fileMatterService, growlService,
+                                          matterCodeService, presetbillService,
+                                          uibDateParser, $uibModal, NgTableParams) 
+  {
+    var self = this;
+    self.userInfo = Auth.getUserInfo();
 
-    .controller('receiptEditCtrl', function($filter, $stateParams, receiptService, $state) {
-        var self = this;
-        self.save = save;
-        self.cancel = cancel;
-        self.isDialog = false;
-        self.viewMode = false;  // for edit / create
+    self.isDialog = false;
+    self.can_edit = $state.$current.data.can_edit;
+    self.isNew = $state.$current.data.can_edit;
 
-        if ($stateParams.id) {
-            receiptService.getItem($stateParams.id)
-            .then(function(item){
-                self.receipt = angular.copy(item);  // important
-            });
-        } else {
-            self.receipt = {};
+    self.itemType = 'All';
+
+    self.queryMatters = function (search) {
+      return fileMatterService.getList(1, 5, search).then(function (resp) {
+        return resp.data
+      })
+    }
+
+    self.matterChange = function (matter) {
+      if (matter && matter.JsonDesc) {
+        self.entity.fileNo = matter.key;
+        var matterInfo = JSON.parse(matter.JsonDesc.replace(/[\u0000-\u0019]+/g,""));
+        var clsPrimaryClient = matterInfo.primaryClient;
+
+        self.entity.matter = matterInfo.matter;
+        self.matterDescription = self.entity.matter.description;
+        if (matterInfo.propertyGroup[0]) {
+          self.entity.strPropertyAddress = matterInfo.propertyGroup[0].fullTitle;
+          self.entity.strState = matterInfo.propertyGroup[0]
         }
+        self.entity.issueToName = clsPrimaryClient.name;
+        self.entity.strClientName = clsPrimaryClient.name;
+      }
+    }
 
-        function save() {
-            receiptService.save(self.receipt).then(function(receipt) {
-                self.receipt = receipt;
-                $state.go('receipts.list');
-            })
-            .catch(function(err){
-                //Handler
-            });
-        }
+    self.presetBillChange = function (item) {
+      if (item && self.entity.strBillName != item.code) {
+        presetbillService.getItem(item.code).then(function (item) {
+          self.entity.listBilledItems = item.listBilledItems;
+          refreshItems();
+        });
+      }
+    }
 
-        function cancel() {
-            $state.go('receipts.list');            
+    self.queryBills = function (keyword) {
+      return presetbillService.getTableList(1, 10, keyword).then(function (resp) {
+        return resp;
+      });
+    }
+
+    self.insert = function (idx) {
+      var modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'billItemModal.html',
+        controller: 'billItemModalCtrl as vm',
+        size: 'lg',
+        keyboard: true,
+        resolve: {
+          state: function () {
+            return self.entity.strState;
+          },
+          category: function () {
+            return null;
+          },
+          excludes: function () {
+            var arr = [];
+            for (ii in self.entity.listBilledItems) {
+              var item = self.entity.listBilledItems[ii];
+              arr.push(item.strItemCode);
+            }
+            return arr;
+          }
         }
-    })
+      }).result.then(function (res) {
+        if (res && res.length > 0) {
+          for (ii in res) {
+            if (idx != -1) {
+              self.entity.listBilledItems.splice(idx+parseInt(ii)+1, 0, res[ii]);
+            } else {
+              self.entity.listBilledItems.push(res[ii]);
+            }
+          }
+          refreshItems();
+        }
+      }, function (res) {});
+    }
+
+    self.move = function (x, y) {
+      if (x < 0) {
+        return;
+      } else if (y == self.entity.listBilledItems.length) {
+        return;
+      }
+
+      var b = self.entity.listBilledItems[y];
+      self.entity.listBilledItems[y] = self.entity.listBilledItems[x];
+      self.entity.listBilledItems[x] = b;
+      self.tableFilter.reload();
+    };
+
+    self.remove = function (code) {
+      for (ii in self.entity.listBilledItems) {
+        var item = self.entity.listBilledItems[ii];
+        if (item.strItemCode == code) {
+          self.entity.listBilledItems.splice(ii, 1);
+          break;
+        }
+      }
+      refreshItems();
+    }
+
+    function initializeTable () {
+      self.tableFilter = new NgTableParams({
+        page: 1,
+        count: 25,
+        sorting: {
+          name: 'asc' 
+        }
+      }, {
+        counts: [],
+        getData: function (params) {
+          return self.entity.listBilledItems.filter(function (item) {
+            return self.itemType == 'All' || item.strBillItemType == self.itemType;
+          })
+        } 
+      });
+      
+      refreshItems();
+    }
+
+    function refreshItems () {
+      self.typeSum = {
+        All: 0.0,
+        Fees: 0.0,
+        Disb: 0.0,
+        DisbWithTax: 0.0
+      };
+
+      self.typeSST = {
+        Fees: 0.0,
+        Disb: 0.0,
+        DisbWithTax: 0.0
+      };
+
+      for (ii in self.entity.listBilledItems) {
+        var item = self.entity.listBilledItems[ii];
+        self.typeSum[item.strBillItemType] += parseFloat(item.decUnitCost);
+        self.typeSST[item.strBillItemType] += parseFloat(item.decUnitCost) * 
+                                              parseFloat(item.decTaxRate);
+        self.typeSum['All'] += parseFloat(item.decUnitCost);
+      }
+
+      self.tableFilter.reload();
+    }
+
+    self.filterItem = function (type) {
+      self.itemType = type;
+      self.tableFilter.reload();
+    }
+
+    if ($stateParams.id) {
+      self.title = 'Edit Receipt';
+      receiptService.getItem($stateParams.id).then(function (item){
+        self.entity = refactorService.preConvert(item, true);
+        self.entity_ = angular.copy(self.entity);
+        self.entity.listBilledItems = [];
+        initializeTable();
+
+        if (self.entity.strBillName) {
+          self.presetCode = {
+            code: self.entity.strBillName
+          }          
+        }
+      });
+    } else {
+      self.title = 'New Receipt';
+      self.entity = {
+        strState: 'Common',
+        dtCreateDate: uibDateParser.parse(new Date()),
+        listBilledItems: []
+      };
+      initializeTable();
+    }
+
+    self.save = function () {
+      entity = refactorService.getDiff(self.entity_, self.entity);
+      receiptService.save(entity, self.entity_).then(function (item) {
+        if (item) {  // ignore when errors
+          if (self.entity_) {
+            $state.reload();
+          } else {
+            $state.go('billing.receipts-edit', { 'id': item.code });
+          }
+          growlService.growl('Saved successfully!', 'success');          
+        }
+      });
+    }
+  })
--- js/services/receipt.js
+++ js/services/receipt.js
@@ -1,94 +1,30 @@
 denningOnline
-    // =========================================================================
-    // Billing Items
-    // =========================================================================
-    
-    .service('receiptService', ['$q', '$timeout', '$http', function($q, $timeout, $http) {
-        var service = {};
+  .service('receiptService', function(http) {
+    var service = {};
 
-        service.receipts = null;
-        service.getList = getList;
-        service.getItem = getItem;
-        service.save = save;
-        service.delete = delete_;
+    service.getList = function (page, pagesize, keyword) {
+      return http.GET('v1/ClientReceipt', {
+        page: page,
+        pagesize: pagesize,
+        search: keyword 
+      }).then(function (resp) {
+        return resp;
+      });
+    }
 
-        function getList() {
-            if (service.receipts) {
-                var deferred = $q.defer();
-                deferred.resolve(service.receipts);
-                return deferred.promise;
-            } else {
-                return $http.get('data/receipts.json')
-                .then(function(resp){
-                    service.receipts = resp.data;                
-                    return resp.data;
-                })                
-            }
-        }
+    service.getItem = function (code) {
+      return http.GET(`v1/ClientReceipt/${code}`).then(function (resp) {
+        return resp.data;
+      });
+    }
 
-        function getItem(code) {
-            if(service.receipts) {
-                var deferred = $q.defer();
-                var item = service.receipts.filter(function(c) {
-                    return c.code == code;
-                });
+    service.save = function (entity) {
+      var method = entity.code ? 'PUT': 'POST';
 
-                if (item.length == 1)
-                    deferred.resolve(item[0]);
-                else
-                    deferred.reject(new Error('No Item with the code'));
+      return http[method]('v1/table/receipt', entity).then(function (resp) {
+        return resp ? resp.data : null;
+      });
+    }
 
-                return deferred.promise;
-            } else {
-                return getList().then(function(data) {
-                    var item = service.receipts.filter(function(c) {
-                        return c.code == code;
-                    });
-
-                    if (item.length == 1)
-                        return item[0];
-                    else
-                        throw new Error('No such item');
-                });
-            }
-        }
-
-        function save(receipt) {
-            var deferred = $q.defer();
-
-            $timeout(function(){
-                var idx = service.receipts.map(function(c) { return c.code; }).indexOf(receipt.code);
-                if(idx != -1) {
-                    service.receipts[idx] = receipt;
-                } else {
-                    // should be done on server side
-                    receipt.code = Math.floor(Math.random() * 1000 + 1);
-                    service.receipts.push(receipt);
-                }
-
-                // @@ send post request to server to save the item
-                deferred.resolve(receipt);
-            }, 100);
-
-            return deferred.promise;
-        }
-
-        function delete_(receipt) {
-            var deferred = $q.defer();
-
-            $timeout(function(){
-                var idx = service.receipts.map(function(c) { return c.code; }).indexOf(receipt.code);
-                if(idx != -1) {
-                    service.receipts.splice(idx, 1);
-                    deferred.resolve(receipt);
-                } else {
-                    defered.reject(new Error('There is no such receipt'));
-                }
-                // @@ send delete request to server to delete the item
-            }, 100);
-
-            return deferred.promise;
-        }
-        return service;
-        
-    }])
+    return service;
+  })
