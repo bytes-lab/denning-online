159543b9120d9484b0d4c8ae7c49621debfc008b
IT-corridor <jason.5001001@gmail.com>
2018-10-29 14:51:14 +0530

    revise project list, service

--- src/js/controllers/project.js
+++ src/js/controllers/project.js
@@ -1,82 +1,26 @@
 denningOnline
-  .controller('projectListCtrl', function($filter, $sce, $uibModal, NgTableParams, projectService) {
+  .controller('projectListCtrl', function(NgTableParams, projectService, $state) {
     var self = this;
-    self.dataReady = false;
-    self.openDelete = openDelete;
 
-    projectService.getList().then(function(data) {
-      self.data = data;
-      self.dataReady = true;
-      initializeTable();
-    });    
-    
-    function initializeTable () {
-      //Filtering
-      self.tableFilter = new NgTableParams({
-        page: 1,      // show first page
-        count: 10,
-        sorting: {
-          name: 'asc'   // initial sorting
-        }
-      }, {
-        total: self.data.length, // length of data
-        getData: function(params) {
-          // use build-in angular filter
-          var orderedData = params.filter() ? $filter('filter')(self.data, params.filter()) : self.data;
-          orderedData = params.sorting() ? $filter('orderBy')(orderedData, params.orderBy()) : orderedData;
-
-          this.new_ic = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
-          this.name = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
-          this.email = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
-          this.phone3 = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
-
-          params.total(orderedData.length); // set total for recalc pagination
-          return this.new_ic, this.id_type, this.name, this.email, this.phone3;
-        }
-      })    
-    }
-
-    self.modalContent = 'Are you sure to delete the contract?';
+    self.tableFilter = new NgTableParams({
+      page: 1,
+      count: 25,
+      sorting: {
+        name: 'asc'
+      }
+    }, {
+      getData: function(params) {
+        return projectService.getList(params.page(), params.count(), self.keyword)
+        .then(function (data) {
+          params.total(data.headers('x-total-count'));
+          return data.data;
+        });
+      }
+    })
   
-    //Create Modal
-    function modalInstances(animation, size, backdrop, keyboard, contact) {
-      var modalInstance = $uibModal.open({
-        animation: animation,
-        templateUrl: 'myModalContent.html',
-        controller: 'projectDeleteModalCtrl',
-        size: size,
-        backdrop: backdrop,
-        keyboard: keyboard,
-        resolve: {
-          contact: function () {
-            return contact;
-          }
-        }
-      
-      });
+    self.search = function () {
+      self.tableFilter.reload();
     }
-    //Prevent Outside Click
-    function openDelete(contact) {
-      modalInstances(true, '', 'static', true, contact)
-    };    
-  })
-
-  .controller('projectDeleteModalCtrl', function ($scope, $uibModalInstance, contact, projectService, $state) {
-    $scope.ok = function () {
-      projectService.delete(contact).then(function(contact) {
-        $state.reload();
-      })
-      .catch(function(err){
-        //Handler
-
-        //$scope.formname.contactInfo.$error.push({meessage:''});
-      });
-      $uibModalInstance.close();
-    };
-
-    $scope.cancel = function () {
-      $uibModalInstance.dismiss('cancel');
-    };
   })
 
   .controller('projectEditCtrl', function($filter, $stateParams, projectService, $state) {
