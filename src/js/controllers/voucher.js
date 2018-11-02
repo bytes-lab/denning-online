987dd369dc6ba44e8242a2b20e41cf05e8b77fec
IT-corridor <jason.5001001@gmail.com>
2018-11-02 16:57:06 +0530

    revise voucher list

--- src/js/controllers/voucher.js
+++ src/js/controllers/voucher.js
@@ -16,11 +16,15 @@ denningOnline
       }
     })
 
-    self.search = function () {
-      self.tableFilter.reload();
+    self.search = function (event, clear) {
+      if(event.which == 13 || clear) { 
+        if (clear) {
+          self.keyword='';
+        }
+        self.tableFilter.reload();
+      }
     }
   })
-
 
   .controller('voucherEditCtrl', function($stateParams, voucherService, $state, Auth,
                                           refactorService, fileMatterService, growlService,
