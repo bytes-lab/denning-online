dabaad8197c2443c3b3f9280e008f0c13ae1cc54
IT-corridor <jason.5001001@gmail.com>
2019-03-18 16:38:43 +0530

    revise legal firm

--- src/js/controllers/legal-firm.js
+++ src/js/controllers/legal-firm.js
@@ -40,6 +40,25 @@ denningOnline
       legalFirmService.getItem(self.entityCode).then(function (item) {
         self.entity = refactorService.preConvert(item, true);
         self.entity_ = angular.copy(self.entity);
+
+        // set country codes
+        if (self.entity.strPhone1CountryCode) {
+          iso2 = self.entity.strPhone1CountryCode.substr(0, 2);
+          $("input[ng-model='vm.entity.strPhone1No']").intlTelInput("setCountry", iso2);
+        }
+        if (self.entity.strPhone2CountryCode) {
+          iso2 = self.entity.strPhone2CountryCode.substr(0, 2);
+          $("input[ng-model='vm.entity.strPhone2No']").intlTelInput("setCountry", iso2);
+        }
+        if (self.entity.strPhone3CountryCode) {
+          iso2 = self.entity.strPhone3CountryCode.substr(0, 2);
+          $("input[ng-model='vm.entity.strPhone3No']").intlTelInput("setCountry", iso2);
+        }
+        if (self.entity.strFax1CountryCode) {
+          iso2 = self.entity.strFax1CountryCode.substr(0, 2);
+          $("input[ng-model='vm.entity.strFax1No']").intlTelInput("setCountry", iso2);
+        }
+
         if (self.entity.strPostCode) {
           self.strPostCode_ = { 
             postcode: self.entity.strPostCode,
@@ -73,6 +92,30 @@ denningOnline
       }
     }
 
+
+    self.fullAddress = function () {
+      fullAddress = '';
+      if (self.entity.strName)
+        fullAddress = self.entity.strName.trim().toUpperCase()+'\n';
+      if (self.entity.strAddressLine1)
+        fullAddress += self.entity.strAddressLine1.trim()+'\n';
+      if (self.entity.strAddressLine2)
+        fullAddress += self.entity.strAddressLine2.trim()+'\n';
+      if (self.entity.strAddressLine3)
+        fullAddress += self.entity.strAddressLine3.trim()+'\n';
+      if (self.entity.strPostCode)
+        fullAddress += self.entity.strPostCode.trim();
+      if (self.entity.strCity)
+        fullAddress += ' ' + self.entity.strCity.trim();
+      fullAddress += ',\n';
+      if (self.entity.strState)
+        fullAddress += self.entity.strState.trim()+',\n';
+      if (self.entity.strCountry)
+        fullAddress += self.entity.strCountry.trim()+'\n';
+
+      return fullAddress;
+    };
+
     self.copy = function () {
       self.isNew = true;
       self.can_edit = true;
@@ -87,6 +130,16 @@ denningOnline
     }
 
     self.save = function () {
+      // get country codes
+      var tmp = $("input[ng-model='vm.entity.strPhone1No']").intlTelInput("getSelectedCountryData");
+      self.entity.strPhone1CountryCode = tmp.iso2.toUpperCase() + '+' + tmp.dialCode;
+      tmp = $("input[ng-model='vm.entity.strPhone2No']").intlTelInput("getSelectedCountryData");
+      self.entity.strPhone2CountryCode = tmp.iso2.toUpperCase() + '+' + tmp.dialCode;
+      tmp = $("input[ng-model='vm.entity.strPhone3No']").intlTelInput("getSelectedCountryData");
+      self.entity.strPhone3CountryCode = tmp.iso2.toUpperCase() + '+' + tmp.dialCode;
+      tmp = $("input[ng-model='vm.entity.strFax1No']").intlTelInput("getSelectedCountryData");
+      self.entity.strFax1CountryCode = tmp.iso2.toUpperCase() + '+' + tmp.dialCode;
+
       entity = refactorService.getDiff(self.entity_, self.entity);
       legalFirmService.save(entity).then(function (item) {
         if (item) {
